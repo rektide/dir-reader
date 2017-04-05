4dcb8fb411df4f1307a10c188d7400bc5a813b38
rektide de la faye <rektide@voodoowarez.com>
2017-04-05 01:43:29 -0400

    Properly invoke super in constructor. Source maps.

--- dir-reader.js
+++ dir-reader.js
@@ -1,35 +1,49 @@
 import { readdir } from 'mz/fs'
 
 export let defaults= {
-	filterName: '.*js'
+	include: '.*js'
 }
 
 export class DirReader extends Promise{
 	constructor( dir, opts){
-		if( dir instanceof String){
-			this.dir= dir
-			dir= undefined
+		super((resolve, reject)=> {
+			_resolve= resolve
+			_reject= reject
+		})
+		var _resolve, _reject
+
+		if( typeof(dir) == 'string'){
+			dir= {dir}
 		}
 		let filterFiles = this.filterFiles.bind( this)
 		Object.assign( this, dir, opts, {filterFiles}, defaults)
-		if( this.filterName instanceof String){
-			this.filterName= new RegExp( this.filterName)
-		}
-		if( this.filterName instanceof RegExp){
-			let filterRegex= this.filterName
-			this.filterName= function( filename){
-				return filterRegex.match( filename)
-			}
-		}
-	}
-	async then( onFulfilled, onRejected){
-		return readdir( dir|| this.dir)
+		readdir( this.dir)
 			.then( this.filterFiles)
-			.then( onFulfilled, onRejected)
+			.then( _resolve, _reject)
 	}
+
 	filterFiles( files){
-		files= files|| []
-		return files.filter( filename=> this.filterName( filename))
+		if( !files){
+			return []
+		}
+		if( this.include&& this.include.constructor == String){
+			this.include = new RegExp( this.include)
+		}
+		if( this.include instanceof RegExp){
+			let include= this.include
+			this.include= file=> include.test( file)
+		}
+		files= this.include? files.filter( this.include): files
+
+		if( this.exclude&& this.exclude.constructor == String){
+			this.exclude = new RegExp( this.exclude)
+		}
+		if( this.exclude instanceof RegExp){
+			let exclude= this.exclude
+			this.exclude= file=> exclude.test( file)
+		}
+		files= this.exclude? files.filter( file=> !this.exclude( file)): files
+		return files
 	}
 }
 
